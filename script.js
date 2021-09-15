aec3ff4866ed2e94723f4d01248be697a875948d
Valiantsin-Kurakevich <valenteen_k@list.ru>
2021-09-15 21:50:16 +0300

    fix: improve code

--- script.js
+++ script.js
@@ -6,19 +6,9 @@ function handleUpdate() {
     `--${this.name}`,
     this.value + suffix
   );
-  //
-  //
-  //   console.log(this.dataset.sizing);
-  console.log(this.name);
-  console.log(this.value);
-  console.log(suffix);
-  console.log(`--${this.name}`, this.value + suffix);
 }
 
 for (let i = 0; i < inputs.length; i++) {
   let input = inputs[i];
-  //   input.addEventListener("change", handleUpdate);
   input.addEventListener("mousemove", handleUpdate);
 }
-
-//
